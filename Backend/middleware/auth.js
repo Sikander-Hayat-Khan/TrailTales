80efb62efc98e03b5e68ce190ca25b7772636ae3
Nouman Ghauri <muhammadnoumanghauri@gmail.com>
2025-12-16 22:37:08 +0500

    Auth

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -1,16 +1,16 @@
-import jwt from "jsonwebtoken"
-
-async function authorization(req,res,next){
-    if (!req.cookies.token){
-        return res.status(401).json({msg: "Not authorized"})
-    }
-    try {
-        const token = jwt.verify(req.cookies.token,process.env.JWT_SECRET)
-        req.user = token
-        next()
-    } catch (error) {
-        return res.status(401).json({msg : "Not authorized"})
-    }
+import jwt from "jsonwebtoken";
+//authentication
+async function authorization(req, res, next) {
+  if (!req.cookies.token) {
+    return res.status(401).json({ msg: "Not authorized" });
+  }
+  try {
+    const token = jwt.verify(req.cookies.token, process.env.JWT_SECRET);
+    req.user = token;
+    next();
+  } catch (error) {
+    return res.status(401).json({ msg: "Not authorized" });
+  }
 }
 
-export default authorization
+export default authorization;
